18217a84e5cd86a292f8ba8a63c8558a017bd568
Oscar Fajardo Fontiveros <oscar.fajardo@sydney.edu.au>
2024-02-20 14:57:23 +1100

    -Removed extra leaked files

--- transmission_models/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from transmission_models.models import *
-from transmission_models.models.didelot_unsampled import *
-# from transmission_models.utils import *
-from transmission_models.utils import tree_to_newick
