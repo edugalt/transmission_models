dcb0aecdbeb11f3949e9465f95e8702bded64204
Oscar Fajardo Fontiveros <oscar.fajardo@sydney.edu.au>
2024-06-20 17:47:27 +1000

    - Corrected issues with imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 from transmission_models.models import *
 from transmission_models.models.didelot_unsampled import *
-# from transmission_models.utils import *
-from transmission_models.utils import tree_to_newick
+from transmission_models.utils import *
+# from transmission_models.utils import tree_to_newick
